4b3ca565c8bcc8148a9e8180c701045b37360362
jagrutipatil <jagrutipatil32@gmail.com>
2017-04-29 00:41:00 -0700

    Big Data Page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/big_data.ejs', function(req, res, next) {
+  res.render('big_data', { title: 'Big Data' });
+});
+
 module.exports = router;
