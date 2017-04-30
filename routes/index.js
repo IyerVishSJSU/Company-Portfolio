8a2c832611fc58dd653fb62115790159264f27d8
Ashwin Angle <ashwinangle301990@gmail.com>
2017-04-30 12:12:13 -0700

    Added Blog and BigData files

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.render('index', { title: 'Express' });
+});
+
+router.get('/big_data.ejs', function(req, res, next) {
+	res.render('big_data', { title: 'Big Data' });
+});
+
+router.get('/blog.ejs', function(req, res, next) {
+	res.render('blog', { title: 'Blog' });
 });
 
 module.exports = router;
